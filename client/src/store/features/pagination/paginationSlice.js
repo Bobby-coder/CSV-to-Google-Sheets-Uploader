b8df4978df9e07e3dfdfa0497ec2d9959f4b0a64
Bobby-coder <bobbysadhwani612@gmail.com>
2024-11-16 00:56:30 +0530

    pagination slice for maintaing current page & limit for data table

---
+++ client/src/store/features/pagination/paginationSlice.js
@@ -0,0 +1,24 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  page: 1,
+  limit: 10,
+};
+
+export const paginationSlice = createSlice({
+  name: "paginationSlice",
+  initialState,
+  reducers: {
+    setPage: (state, action) => {
+      state.page = action.payload;
+    },
+
+    setLimit: (state, action) => {
+      state.limit = action.payload;
+    },
+  },
+});
+
+export const { setPage, setLimit } = paginationSlice.actions;
+
+export default paginationSlice.reducer;
