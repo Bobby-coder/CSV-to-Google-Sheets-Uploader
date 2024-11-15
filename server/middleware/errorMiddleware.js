19d7b33400f727a7cdd47e97dc4c216f1d9a343b
Bobby-coder <bobbysadhwani612@gmail.com>
2024-11-15 13:28:38 +0530

    Error middleware implemented for sending error response

---
+++ server/middleware/errorMiddleware.js
@@ -0,0 +1,11 @@
+function errorMiddleware(err, req, res, next) {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal server error";
+
+  return res.status(statusCode).json({
+    success: false,
+    message: message,
+  });
+}
+
+export default errorMiddleware;
